af958e963f468e9618da2f6a9f3b10a333b4ff73
Arief Fauzi <arief.fauzi-consultant@prudential.co.id>
2023-03-28 21:57:31 +0700

    init project

---
+++ src/contact.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Contact</h2>
+        <p>Email: info@email.com <br />
+           Phone: 002938293089
+</p>
+      </div>
+    );
+  }
+}
+ 
+export default Contact;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDom from "react-dom"
+import Main from "./main"
+import "./style.css"
+
+ReactDom.render(<Main/>,document.getElementById("root"))
